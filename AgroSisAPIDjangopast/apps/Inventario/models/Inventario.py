929ff4321f5e3efd744f1a9bdaea7b58e60edc38
juancc9 <sicsound00@gmail.com>
2025-03-21 09:01:01 -0500

    "Ajustes a los valores traidos por formularios"

--- AgroSisAPIDjangopast/apps/Inventario/models/Inventario.py
+++ AgroSisAPIDjangopast/apps/Inventario/models/Inventario.py
@@ -4,5 +4,6 @@ from .Insumo import Insumo
 class Inventario(models.Model):
     fk_herramienta = models.ForeignKey(Herramienta,on_delete=models.SET_NULL,null=True)
     fk_insumo = models.ForeignKey(Insumo,on_delete=models.SET_NULL,null=True)
+    
     def __str__(self):
         return str(self.fk_herramienta) + str(self.fk_insumo)
--- AgroSisAPIDjangopast/apps/Inventario/serializers/InventarioSerializer.py
+++ AgroSisAPIDjangopast/apps/Inventario/serializers/InventarioSerializer.py
@@ -1,7 +1,10 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from ..models.Inventario import Inventario
 
-class InventarioSerializer(ModelSerializer):
+class InventarioSerializer(serializers.ModelSerializer):
+    fk_herramienta_nombre = serializers.CharField(source="fk_herramienta.nombre", read_only=True)
+    fk_insumo_nombre = serializers.CharField(source="fk_insumo.tipo", read_only=True)
+
     class Meta:
         model = Inventario
-        fields = '__all__'
+        fields = "__all__"
